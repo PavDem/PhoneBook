0e218ccda0be3420d7af0c692d99b4dd0d3b4167
Pheerie <piri.komp@gmail.com>
2020-10-28 07:45:01 +0300

    finished method add and list

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -10,26 +10,7 @@ public class PhoneBook {
         String name;
         String number;
         String[][] book = new String[5][2];
-        //Первая колонка - name, вторая - number
 
-        name = scanner.next();
-
-        if (!checkName(name))
-            return;
-
-
-
-        name = formatName(name);
-
-
-        if (!checkPhoneNumber(name))
-            return;
-
-        number = scanner.next();
-
-        number = formatPhoneNumber(number);
-
-        add(book, name, number);
 
     }
 
@@ -51,16 +32,51 @@ public class PhoneBook {
     }
 
     public static void add(String[][] book, String name, String number) {
+        //if last index elements(columns) don't equal null, create new array with original row length + 5 new rows
+        if (book[book.length - 1][0] != null && book[book.length - 1][1] != null) {
+            int newLength = book.length + 5;
+            String[][] newBook = new String[newLength][2];
+
+            //copy old records to new array
+            for (int i = 0; i < book.length; i++) {
+                newBook[i][0] = book[i][0];
+                newBook[i][1] = book[i][1];
+            }
+
+            //change to old name (newBook -> book)
+            book = newBook;
+        }
+
+        //search for next empty(null) index
+        for (int i = 0; i < book.length; i++) {
+            //if, for some reason, row has only a name or a number, we treat it as it was empty
+            if (book[i][0] == null || book[i][1] == null) {
+                //add name and number
+                book[i][0] = name;
+                book[i][1] = number;
+                break;
+            }
+        }
+
         //add logic
     }
 
     public static void list(String[][] book) {
         //print phone book
+        for (int i = 0; i < book.length; i++) {
+            String name = book[i][0];
+            String number = book[i][1];
+            //check if both name and number filled, don't show otherwise
+            if (name != null && number != null) {
+                String message = "Name is " + name + ", number is" + " " + number;
+                System.out.println(message);
+            }
+        }
     }
 
     public static int findIndexByName(String[][] book, String name) {
         for (int i = 0; i < book.length; i++) {
-            if(book[i][0] == name) {
+            if (book[i][0] != null && book[i][0].equals(name)) {
                 return i;
             }
         }
