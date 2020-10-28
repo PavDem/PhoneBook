fe83ba032017226b653866f27e14784394c01d55
Pheerie <piri.komp@gmail.com>
2020-10-28 06:47:54 +0300

    Added int GetIndexByName method

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+
+
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String number;
+        String[][] book = new String[5][2];
+        //Первая колонка - name, вторая - number
+
+        name = scanner.next();
+
+        if (!checkName(name))
+            return;
+
+
+
+        name = formatName(name);
+
+
+        if (!checkPhoneNumber(name))
+            return;
+
+        number = scanner.next();
+
+        number = formatPhoneNumber(number);
+
+        add(book, name, number);
+
     }
 
-    public static boolean checkPhoneNumber(String phoneNumber) {
+
+    public static boolean checkName(String name) {
         return true;
     }
 
-    public static boolean checkName(String name) {
+    public static boolean checkPhoneNumber(String phoneNumber) {
         return true;
     }
 
@@ -27,4 +57,14 @@ public class PhoneBook {
     public static void list(String[][] book) {
         //print phone book
     }
+
+    public static int findIndexByName(String[][] book, String name) {
+        for (int i = 0; i < book.length; i++) {
+            if(book[i][0] == name) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
